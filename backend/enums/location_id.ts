bfe0c81f0c5916bfa2eab553a8c3072977444201
Nicholas Chan <nicholasch.an@outlook.com>
2025-10-04 14:30:51 -0700

    added backend stuff

---
+++ backend/enums/location_id.ts
@@ -0,0 +1,10 @@
+enum LocationId {
+  LIBRARY = 1,
+  ASB = 2,
+  WMC = 3,
+  DORM = 4,
+  LAB = 5,
+  CLASSROOM = 6,
+}
+
+export default LocationId;
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,25 @@
 import express from 'express';
+import { Server as SocketIOServer } from "socket.io";
 
-const app = express();
+const server = express();
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// WebSocket 
+const io = new SocketIOServer(server, {
+    // cors: {
+    //     origin: [process.env.CLIENT_URL],
+    //     credentials: true,
+    // }
+});
 
-app.listen(port, () => {
+server.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+server.get('/locate', (req, res) => {
+  
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
