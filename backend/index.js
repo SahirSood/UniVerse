a34bbf044e5ea289bb1360e984e6019f312662aa
Nicholas Chan <nicholasch.an@outlook.com>
2025-10-04 15:43:04 -0700

    ok fixes for real

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,29 @@
 import express from 'express';
-import findNearestPolygon from './helpers/LocationToRoom';
-import SocketServerInit from './helpers/WebSockets';
+import http from 'http';
+import findNearestPolygon from './helpers/LocationToRoom.js';
+import SocketServerInit from './helpers/WebSockets.js';
 
 // location imports
-import geojson from "../locations/SFU.geojson"
-
+import fs from "fs";
+const geojson = JSON.parse(
+  fs.readFileSync(new URL("./locations/SFU.geojson", import.meta.url), "utf8")
+);
 
 // Server starting
-const server = express();
+const app = express();
+const server = http.createServer(app);
 const port = 3000
 const socketServer = SocketServerInit(server, port)
 
-
-server.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
-server.get('/locations', (req, res) => {
+app.get('/locations', (req, res) => {
   const location = findNearestPolygon(geojson, req.lat, req.lon);
   res.send(location);
 });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
