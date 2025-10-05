66cca71ede9d7108acb518525fadc2defe96230f
gurjas-s <gurjas35@gmail.com>
2025-10-04 17:35:24 -0700

    added basic interactive map

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,9 +1,45 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { Geojson } from 'react-native-maps';
+import geojsonData from '../../assets/SFU.json';
 
-export default function MapScreen() {
+export default function TestScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Map Screen</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Geojson
+          geojson={geojsonData as any}
+          strokeColor="red"
+          fillColor="rgba(255,0,0,0.3)"
+          strokeWidth={2}
+        />
+      </MapView>
+    
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+  text: {
+    position: 'absolute',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+});
